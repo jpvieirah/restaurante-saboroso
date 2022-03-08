081db9a882eab440499cd2ada262f66cf4f30c7d
helsin <jp201101@gmail.com>
2022-03-08 12:36:26 -0300

    Organizando o Conteúdo com Includes

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,35 +1,37 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var menus = require('./../inc/menus');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-  
-  SELECT * FROM tb_menus ORDER BY title
-
-  `, (err, results)=>{
-
-    if(err){
-      console.log(err);
-    }
+  menus.getMenus().then(results => {
 
     res.render('index', { 
       title: 'Restaurante Saboroso!',
       menus: results
   });
+
   });
+
 });
 
 router.get('/menus', function(req, res, next){
 
-  res.render('menus', {
-    title: 'menus - Restaurante Saboroso!',
-    background: 'images/img_bg_1.jpg',
-    h1: 'Saboreie nosso menu!'
+  menus.getMenus().then(results => {
+
+    res.render('menus', {
+      title: 'menus - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu!',
+      menus: results
+    });
+  
+
   });
 
+ 
 });
 
 router.get('/reservations', function(req, res, next){
